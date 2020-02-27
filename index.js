fcc08f65aef06fa399e52860ced18474a5633f92
Joshua Hunt <jelijahhunt@gmail.com>
2020-02-27 20:34:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,3 +15,8 @@ function destructivelyPrependKitten(name) {
 function destructivelyRemoveLastKitten(){
 kittens.pop()
 return kittens}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
