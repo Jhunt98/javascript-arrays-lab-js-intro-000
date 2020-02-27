1b37b9573dfdc359ad7a2fe2e2510584b533b6e0
Joshua Hunt <jelijahhunt@gmail.com>
2020-02-27 20:29:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var kittens = ['Milo', 'Otis', 'Garfield'] //define your array here
 // Add your functions and code here
 
 function destructivelyAppendKitten(name) {
-  name.push('Ralph')
-  return name
+  destructivelyAppendKitten.push('Ralph')
+  return destructivelyAppendKitten
 }
